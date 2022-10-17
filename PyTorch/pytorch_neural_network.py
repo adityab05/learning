2d3a654e1c08d29f0e39ee53fe601bb897c8f9b7
Aditya Badve <abadv001@ucr.edu>
2022-10-16 22:40:38 -0700

    add basic neural network template

---
+++ PyTorch/pytorch_neural_network.py
@@ -0,0 +1,105 @@
+"""
+# PyTorch Basic Neural Network Template
+"""
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Dummy Vars
+train_loader = ""
+test_loader = ""
+
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# Hyper-parameters 
+num_epochs = 5
+batch_size = 4
+learning_rate = 0.001
+
+
+# Fully Connected Network
+class FCN(nn.Module):
+    def __init__(self):
+        super(FCN, self).__init__()
+        self.fc1 = nn.Linear(18, 1024)
+        self.fc2 = nn.Linear(1024, 256)
+        self.fc3 = nn.Linear(256, 20)
+        
+
+    def forward(self, x):
+        output = F.relu(self.fc1(x))
+        output = F.relu(self.fc2(output))   
+        output = F.softmax(self.fc3(output))
+        return output
+
+
+# Model
+model = FCN().to(device)
+
+
+# Loss and Optimizer
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+
+# Train Model
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (features, labels) in enumerate(train_loader):
+        features = features.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(features)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 2000 == 0:
+            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
+
+print('Finished Training')
+
+
+# Evaluate Model
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    n_class_correct = [0 for i in range(10)]
+    n_class_samples = [0 for i in range(10)]
+    for features, labels in test_loader:
+        features = features.to(device)
+        labels = labels.to(device)
+        outputs = model(features)
+        
+        # max returns (value ,index)
+        _, predicted = torch.max(outputs, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+        
+        for i in range(batch_size):
+            label = labels[i]
+            pred = predicted[i]
+            if (label == pred):
+                n_class_correct[label] += 1
+            n_class_samples[label] += 1
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network: {acc} %')
+
+    for i in range(10):
+        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+        print(f'Accuracy of {classes[i]}: {acc} %')
